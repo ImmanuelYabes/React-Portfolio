882ee80d4113f5e0ef186c88ef64de7fef59c4ca
Yabes123 <immanuelyabes20@gmail.com>
2023-11-23 05:01:34 +0700

    g

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./app.scss";
 import Contact from "./components/contact/Contact";
 import Hero from "./components/hero/Hero";
-import Navbar from "./components/navbar/navbar";
+import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Skills from "./components/skills/Skills";
